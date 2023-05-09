e3a5cf720fa09dfd018fd7b9d31e98bbef17d6c3
Yago <peixinhoyago@gmail.com>
2023-05-09 18:06:30 -0300

    :construction: wip: Working on Error Handling

--- app/api/token.py
+++ app/api/token.py
@@ -4,19 +4,19 @@ from app.models import User
 import datetime
 import os
 import jwt
+from app.api.errors import APIAuthError, APIError
 
 
 @bp.route('/token', methods=['POST'])
 def login():
     body = request.get_json()
     
-    user = User.query.filter_by(email=body['email']).first_or_404()
+    user = User.query.filter_by(email=body['email']).first()
     
-    if not user.verify_password(body['password']):
-        return jsonify({
-            "message": "Email or Password invalid"
-        }), 403
-        
+    if not hasattr(user, 'verify_password'): 
+        raise APIError('E-mail ou senha inválido', 400)
+
+
     payload = {
         'id': user.id,
         'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
